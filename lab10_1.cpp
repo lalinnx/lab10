085318f3f4b72fa70c0b8c2ae8c0fb28ce8ad3f3
lalinnx <lalinpye@gmail.com>
2021-01-12 16:48:35 +0700

    finish

--- lab10_1.cpp
+++ lab10_1.cpp
@@ -1,2 +1,19 @@
-// cout << "Press Enter 3 times to reveal your future.";
-// cout << "You will get A in this 261102.";
+#include<iostream>
+#include<string>
+#include<ctime>
+#include<cstdlib>
+using namespace std;
+
+int main(){
+    cout << "Press Enter 3 times to reveal your future.";
+    cin.get();
+    cin.get();
+    cin.get();
+    
+    string x[]={"A","B+","B","C+","C","D+","D","F","W"};
+    srand(time(0));
+    int i = rand()%9;
+    cout << "You will get " << x[i] << " in this 261102.";
+    
+    return 0;
+}
